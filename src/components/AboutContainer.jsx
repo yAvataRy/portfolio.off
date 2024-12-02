04aae31480f350afd1868a3d7c706c3ca8d6d93d
Vinicius Lima <vinicius@pulchercode.com>
2024-12-02 12:59:43 -0300

    um pouco de SASS e about

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,10 +3,10 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-      Sou desenvolvedor em início de carreira, com experiência em tecnologias de Front-end. Atualmente, estou me aprofundando no Nuxt.js, e iniciando em Python.
+      Sou desenvolvedor em início de carreira, com experiência em tecnologias Front-end. Atualmente, estou me aprofundando em Nuxt.js e iniciando em Python.
       </p>
       <p>
-      Em 2023, comecei a migração para area de tecnologia e entrei na faculdade, em 2024 iniciei a migração para a área de desenvolvimento, focando no aprendizado contínuo. Tenho grande interesse em trabalhar com inteligência artificial e automação de processos, aplicando essas tecnologias para otimizar decisões. Busco oportunidades para consolidar meus conhecimentos e contribuir para o sucesso de projetos desafiadores e transformadores.
+      Em 2023, comecei a migração para área de tecnologia iniciando a faculdade de Sistemas Para Internet. Em 2024 comecei na área de desenvolvimento, focado no aprendizado contínuo. Tenho grande interesse em trabalhar com inteligência artificial e automação de processos, aplicando essas tecnologias para otimizar decisões. Busco oportunidades para consolidar meus conhecimentos e contribuir para o sucesso de projetos desafiadores e transformadores.
       </p>
     </section>
   );
