a4abcbb3d930b1960196a9597c3a7e0be4afb4c9
Vinicius Lima <vinicius@pulchercode.com>
2024-12-02 13:05:19 -0300

    teste

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,7 +6,7 @@ const AboutContainer = () => {
       Sou desenvolvedor em início de carreira, com experiência em tecnologias Front-end. Atualmente, estou me aprofundando em Nuxt.js e iniciando em Python.
       </p>
       <p>
-      Em 2023, comecei a migração para área de tecnologia iniciando a faculdade de Sistemas Para Internet. Em 2024 comecei na área de desenvolvimento, focado no aprendizado contínuo. Tenho grande interesse em trabalhar com inteligência artificial e automação de processos, aplicando essas tecnologias para otimizar decisões. Busco oportunidades para consolidar meus conhecimentos e contribuir para o sucesso de projetos desafiadores e transformadores.
+      Em 2023, comecei a migração para área de tecnologia iniciando a faculdade de Sistemas Para Internet. Em 2024 comecei na área de desenvolvimento, focado no aprendizado contínuo. Tenho grande interesse em trabalhar com inteligência artificial e automação de processos, aplicando essas tecnologias para otimizar decisões. Busco oportunidades para consolidar meus conhecimentos e contribuir para o sucesso de projetos desafiadores e transformadores. 
       </p>
     </section>
   );
